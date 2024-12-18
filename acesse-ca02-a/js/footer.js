e7626dc334287aef385cb55008a7c5406383a50d
jotainvest <108202340+jotainvest@users.noreply.github.com>
2024-12-18 13:06:06 -0300

    Add files via upload

---
+++ acesse-ca02-a/js/footer.js
@@ -0,0 +1,4 @@
+var date=new Date,year=date.getFullYear();$(".footer").append('<img src="https://publifile.s3.amazonaws.com/marca/logo.svg" alt="" class="logoJota"><p>ESTE SITE NÃO É do FACEBOOK: Este site não faz parte do site do Facebook ou do Facebook Inc. Além disso, este site NÃO é endossado pelo Facebook de nenhuma maneira. FACEBOOK é uma marca comercial independente da FACEBOOK, Inc.</p><p>Os resultados referentes ao serviço foram avaliados e comprovados por nossos clientes. Não garantimos os resultados iguais para todos, estes variam de pessoa para pessoa dependendo de diversos fatores individuais. As informações fornecidas neste site destina-se ao seu conhecimento geral e não garantimos um resultado específico, todo resultado será de inteira responsabilidade do cliente. Essa página/produto não tem vinculo com o Facebook..</p><p>Copyright © '+year+"  - Todos os direitos reservados.</p>");
+
+
+
